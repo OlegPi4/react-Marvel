c2f136e0c8b2889755acabb84da9761cad8587d6
OlegPichkur <oleg1pichkur@gmail.com>
2023-08-09 14:17:12 +0300

    реализ.  LoadMore  и проверка пропсов  PropTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
 import { ErrorMessage } from 'react';
@@ -118,4 +120,8 @@ const View = ({ char }) => {
         </>
     )
 }
+CharInfo.propTypes = {
+    charId: PropTypes.number 
+}
+
 export default CharInfo;
